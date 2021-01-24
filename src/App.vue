891ec4aeddfb754fbad68d568bc611475ec081e5
harimbu <harimbu@gmail.com>
2021-01-24 16:38:54 +0900

    vuetity

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <v-app>
-    <v-app-bar color="white" app flat>
+    <v-app-bar color="white" app flat absolute>
       <Header />
     </v-app-bar>
-    <v-main>
-      <router-view></router-view>
-    </v-main>
+    <router-view></router-view>
     <v-footer padless color="white">
       <Footer />
     </v-footer>
